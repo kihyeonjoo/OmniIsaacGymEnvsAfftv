96a08453e2bce8a4ef494d8b509e1aa67ea94919
kihyeonjoo <kihyeonjoo@gmail.com>
2024-09-03 17:01:42 +0900

    240903 we almost complete the reward, observation!

--- omniisaacgymenvs/tasks/franka_pickandplace.py
+++ omniisaacgymenvs/tasks/franka_pickandplace.py
@@ -52,15 +52,34 @@ class FrankaPickAndPlaceTask(RLTask):
         self._cube_id = None                            # Actor ID corresponding to cube for a given env
         self._target_id = None                          # Actor ID corresponding to target for a given env
 
-        self.cube_size = 0.050                          # cube size
-        self.target_size = 0.050                        # target size
+        self._cube_size = 0.050                          # cube size
+        self._target_size = 0.050                        # target size
         
-        self._eef_state = None
+        self._eef_state = None                          # Current state of end effector
+        self._lf_state = None                       # Current state of left finger
+        self._rf_state = None                       # Current state of left finger
 
         # Tensor placeholders
-        self._root_state = None         # 이런거 필요없는듯                # 
-        self._dof_state = None
+        self._j_eef = None                              # Jacobian for end effector
+        self._mm = None                                 # Mass matrix
 
+        self._arm_control = None                        # Tensor buffer for contolling arm
+        self._gripper_control = None                    # Tensor buffer for controlling gripper
+        self._pos_control = None                        # Position actions
+        self._effort_control = None                     # Torque actions
+
+        self._franka_effort_limits = None               # Actuator effort limits for franka
+        self._global_indices = None                     # Unique indices corresponding to all envs in flattened array -> ?
+
+        # OSC Gains
+        self.kp = to_torch([150.] * 6, device=self.device)
+        self.kd = 2 * torch.sqrt(self.kp)
+        self.kp_null = to_torch([10.] * 7, device=self.device)
+        self.kd_null = 2 * torch.sqrt(self.kp_null)
+
+        # Set control limits
+        self.cmd_limit = to_torch([0.1, 0.1, 0.1, 0.5, 0.5, 0.5], device=self.device).unsqueeze(0) if \
+        self.control_type == "osc" else self._franka_effort_limits[:7].unsqueeze(0)
 
         RLTask.__init__(self, name, env)
         return
@@ -91,6 +110,13 @@ class FrankaPickAndPlaceTask(RLTask):
         self.start_position_noise = self._task_cfg["env"]["startPositionNoise"]
         self.start_rotation_noise = self._task_cfg["env"]["startRotationNoise"]
 
+        self.reward_settings = {
+            "r_dist_scale": self._task_cfg["env"]["distRewardScale"],
+            "r_lift_scale": self._task_cfg["env"]["liftRewardScale"],
+            "r_align_scale": self._task_cfg["env"]["alignRewardScale"],
+            "r_stack_scale": self._task_cfg["env"]["stackRewardScale"],
+        }
+
     def set_up_scene(self, scene) -> None:
         # Set up the scene with the robot and objects
         self.get_franka()
@@ -104,6 +130,9 @@ class FrankaPickAndPlaceTask(RLTask):
         self._target = RigidPrimView(prim_paths_expr="/World/envs/.*/target", name="target_view")
 
         scene.add(self._frankas)
+        scene.add(self._frankas._hands)
+        scene.add(self._frankas._lfingers)
+        scene.add(self._frankas._rfingers)
         scene.add(self._cubes)
         scene.add(self._target)
 
@@ -118,6 +147,9 @@ class FrankaPickAndPlaceTask(RLTask):
         self._target = RigidPrimView(prim_paths_expr="/World/envs/.*/target", name="target_view")
 
         scene.add(self._frankas)
+        scene.add(self._frankas._hands)
+        scene.add(self._frankas._lfingers)
+        scene.add(self._frankas._rfingers)
         scene.add(self._cubes)
         scene.add(self._target)
 
@@ -134,7 +166,7 @@ class FrankaPickAndPlaceTask(RLTask):
         # Set up cube info
         cube_pos = torch.tensor([0.0515, 0.0, 0.7172])
         cube_color = torch.tensor([0.8, 0.2, 0.2])
-        cube_size = 0.05
+        cube_size = self._cube_size
         
         # Load dynamic cube object
         cube = DynamicCuboid(
@@ -153,7 +185,7 @@ class FrankaPickAndPlaceTask(RLTask):
         # Set up target info
         target_pos = torch.tensor([0.0915, 0.0, 0.7172])
         target_color = torch.tensor([0.2, 0.8, 0.2])
-        target_size = 0.05
+        target_size = self._target_size
 
         # Load target object as a fixed visual marker
         target = DynamicCuboid(
@@ -169,7 +201,7 @@ class FrankaPickAndPlaceTask(RLTask):
         # get environment local position
         def get_env_local_pose(env_pos, xformable, device):
             """Compute pose in env-local coordinates"""
-            world_transform = xformable.ComputerLocalToWorldTransform(0)
+            world_transform = xformable.ComputeLocalToWorldTransform(0)
             world_pos = world_transform.ExtractTranslation()
             world_quat = world_transform.ExtractRotationQuat()
 
@@ -198,12 +230,12 @@ class FrankaPickAndPlaceTask(RLTask):
             UsdGeom.Xformable(stage.GetPrimAtPath("/World/envs/env_0/franka/panda_link7")),
             self._device,
         )
-        self._eef_lf_state = get_env_local_pose(
+        self._lf_state = get_env_local_pose(
             self._env_pos[0],
             UsdGeom.Xformable(stage.GetPrimAtPath("/World/envs/env_0/franka/panda_leftfinger")),
             self._device,
         )
-        self._eef_rf_state = get_env_local_pose(
+        self._rf_state = get_env_local_pose(
             self._env_pos[0],
             UsdGeom.Xformable(stage.GetPrimAtPath("/World/envs/env_0/franka/panda_rightfinger")),
             self._device,
@@ -224,36 +256,40 @@ class FrankaPickAndPlaceTask(RLTask):
 
         # eef_state 선언 (px, py, pz, qw, qx, qy, qz)
         self._eef_state = torch.zeros(7, device=self._device)
-        self._eef_state[0:3] = (self._eef_lf_state[0:3] + self._eef_rf_state[0:3]) / 2.0
-        self._eef_state[3:7] = self._eef_lf_state[3:7]
+        self._eef_state[0:3] = (self._lf_state[0:3] + self._rf_state[0:3]) / 2.0
+        self._eef_state[3:7] = self._lf_state[3:7]
         
+        self.num_dofs = 9
         # Jacobian 선언 ()
 
         # hand_pose 선언 (px, py, pz, qw, qx, qy, qz)
-        hand_pose_inv_rot, hand_pose_inv_pos = tf_inverse(hand_pose[3:7], hand_pose[0:3])
+        #hand_pose_inv_rot, hand_pose_inv_pos = tf_inverse(hand_pose[3:7], hand_pose[0:3])
 
         # Initialize states
         self.states.update({
-            "cube_size": torch.ones_like(self._eef_state[0]) * self.cube_size,
-            "target_size": torch.ones_like(self._eef_state[0]) * self.target_size,
+            "cube_size": torch.ones_like(self._eef_state[0]) * self._cube_size,
+            "target_size": torch.ones_like(self._eef_state[0]) * self._target_size,
         })
 
         # Initialize actions
         self.actions = torch.zeros((self._num_envs, self.num_actions), device=self._device)
-        self._pos_control = torch.zeros((self._num_envs, self.num_dofs), dtype=torch.float, device=self._device)
-        self._effort_control = torch.zeros_like(self._pos_control)
+        #self._pos_control = torch.zeros((self._num_envs, self.num_dofs), dtype=torch.float, device=self._device)
+        #self._effort_control = torch.zeros_like(self._pos_control)
 
         # Initialzie control
         # self._arm_control = self._effort_control
 
         # Initialize simulation data for task
-        self.franka_dof_pos = torch.zeros((self._num_envs, self._frankas.num_dof), device=self._device)
-        dof_limits = self._frankas.get_dof_limits()
-        self.franka_dof_lower_limits = dof_limits[0, :, 0].to(device=self._device)
-        self.franka_dof_upper_limits = dof_limits[0, :, 1].to(device=self._device)
-        self.franka_dof_speed_scales = torch.ones_like(self.franka_dof_lower_limits)
-        self.franka_dof_speed_scales[self._frankas.gripper_indices] = 0.1
-        self.franka_dof_targets = torch.zeros((self._num_envs, self._frankas.num_dof), dtype=torch.float, device=self._device)
+        self.franka_dof_pos = torch.tensor(
+            [1.157, -1.066, -0.155, -2.239, -1.841, 1.003, 0.469, 0.035, 0.035], device=self._device
+        )
+
+        # dof_limits = self._frankas.get_dof_limits()
+        # self.franka_dof_lower_limits = dof_limits[0, :, 0].to(device=self._device)
+        # self.franka_dof_upper_limits = dof_limits[0, :, 1].to(device=self._device)
+        # self.franka_dof_speed_scales = torch.ones_like(self.franka_dof_lower_limits)
+        # self.franka_dof_speed_scales[self._frankas.gripper_indices] = 0.1
+        # self.franka_dof_targets = torch.zeros((self._num_envs, self._frankas.num_dof), dtype=torch.float, device=self._device)
 
     def get_observations(self) -> dict:
         """기존에는 observation을 할 때 refresh function을 직접 개발해서 states들을 update하였으나,
@@ -261,6 +297,9 @@ class FrankaPickAndPlaceTask(RLTask):
         self.states에 값들 update하는 것도 Load 후에 하면 좋을듯? """
 
         ##### Load States #####
+        # dof state
+        franka_dof_pos = self._frankas.get_joint_positions(clone=False)
+        franka_dof_vel = self._frankas.get_joint_velocities(clone=False)
         # hand state
         hand_pos, hand_rot = self._frankas._hands.get_world_poses(clone=False)
         # lfinger state
@@ -268,45 +307,47 @@ class FrankaPickAndPlaceTask(RLTask):
         # rfinger state
         self.franka_rfinger_pos, self.franka_rfinger_rot = self._frankas._rfingers.get_world_poses(clonse=False)
         # cube state
-        cube_pos, cube_rot = self._cubes.get_world_poses(clone=False)
+        self.cube_pos, self.cube_rot = self._cubes.get_world_poses(clone=False)
         # target state
-        target_pos, target_rot = self._target.get_world_poses
-        # dof state
-        franka_dof_pos = self._frankas.get_joint_positions(clone=False)
-        franka_dof_vel = self._frankas.get_joint_velocities(clone=False)
+        self.target_pos, self.target_rot = self._target.get_world_poses
+
         # eef state
-        eef_pos = self._eef_state[:3]
-        eef_rot = self._eef_state[3:7]
+        self._eef_state[:3] = (self.franka_lfinger_pos + self.franka_rfinger_pos) / 2.0
+        self._eef_state[3:7] = self.franka_lfinger_rot
 
         ##### Update States #####
         self.states.update({
             # Franka
+            "q": franka_dof_pos,
+            "q_gripper": franka_dof_pos[-2:],
             "eef_pos": self._eef_state[:3],
             "eef_rot": self._eef_state[3:7],
-            "eef_lf_pos": self._eef_lf_state[:3]
+            "eef_lf_pos": self.franka_lfinger_pos,
+            "eef_lf_rot": self.franka_lfinger_rot,
+            "eef_rf_pos": self.franka_rfinger_pos,
+            "eef_rf_rot": self.franka_rfinger_rot,
+            # Cube
+            "cube_pos": self.cube_pos,
+            "cube_rot": self.cube_rot,
+            # Target
+            "target_pos": self.target_pos,
+            "target_rot": self.target_rot,
+            # relative pos (eef - cube)
+            "cube_eef_pos": self.cube_pos - self._eef_state[:3],
+            # cube to target pos
+            "cube_target_pos": self.cube_pos - self.target_pos
         })
 
-        ##### Calculate #####
-        # cube_to_target_pos
-        cube_to_target_pos = torch.norm(cube_pos - target_pos)
-
         ##### Update obs_buf #####
-        """ cube_pos, cube_rot, cube_to_target_pos, eef_pos, eef_rot, q_gripper"""
-        self.obs_buf = torch.cat(
-            (
-                cube_pos,
-                cube_rot,
-                cube_to_target_pos,
-                eef_pos,
-                eef_rot,
-                ### What is q_gripper??? ###
-            ),
-            dim=-1,
-        )
+        """ cube_pos, cube_rot, cube_target_pos, eef_pos, eef_rot, q_gripper"""
+        obs = ["cube_pos", "cube_rot", "cube_target_pos", "eef_pos", "eef_rot"]
+        obs += ["q_gripper"] if self.control_type == "osc" else ["q"]
+        self.obs_buf = torch.cat([self.states[ob] for ob in obs], dim=-1),
 
         observations = {self._frankas.name: {"obs_buf": self.obs_buf}}
         return observations
 
+        ##### 수정 필요 #####
     def pre_physics_step(self, actions) -> None:
         # Apply actions to the simulation
         if not self.world.is_playing():
@@ -323,6 +364,11 @@ class FrankaPickAndPlaceTask(RLTask):
 
         self._frankas.set_joint_position_targets(self.franka_dof_targets, indices=env_ids_int32)
 
+        ##### 수정 필요 #####
+    def post_physics_step(self):
+        self.progress_buf += 1
+        
+        ##### 수정 필요 #####
     def reset_idx(self, env_ids):
         # Reset specific environments
         indices = env_ids.to(dtype=torch.int32)
@@ -354,13 +400,41 @@ class FrankaPickAndPlaceTask(RLTask):
         self.reset_buf[env_ids] = 0
         self.progress_buf[env_ids] = 0
 
+        ##### 필요한 인자들 선언 #####
+        """
+        q, qd, _j_eef
+        """
+    def _computer_osc_torques(self, dpose):
+        q, qd = self._q[:7], self._qd[:7]
+        mm_inv = torch.inverse(self._mm)
+        m_eef_inv = self._j_eef @ mm_inv @ torch.transpose(self._j_eef, 1, 2)
+        m_eef = torch.inverse(m_eef_inv)
+
+        u = torch.transpose(self._j_eef, 1, 2) @ m_eef @ (
+            self.kp * dpose - self.kd * self.states["eef_vel"]).unsqueeze(-1)
+        
+        j_eef_inv = m_eef @ self._j_eef @ mm_inv
+        u_null = self.kd_null * -qd + self.kp_null * (
+            (self.franka_default_dof_pos[:7] - q + np.pi) % (2 * np.pi) - np.pi)
+        u_null[:7] *= 0
+        u_null = self._mm @ u_null.unsqueeze(-1)
+        u += (torch.eye(7, device=self.device).unsqueeze(0) - torch.transpose(self._j_eef, 1, 2) @ j_eef_inv) @ u_null
+
+        u = tensor_clamp(u.squeeze(-1),
+                         -self._franka_effort_limits[:7].unsqueeze(0), self._franka_effort_limits[:7].unsqueeze(0))
+        
+        return u
+
     def calculate_metrics(self) -> None:
         # Reward function for Pick-and-Place task
-        dist_to_target = torch.norm(self._cubes.get_world_poses(clone=False)[0] - self._targets.get_world_poses(clone=False)[0], dim=-1)
-        reward = -dist_to_target  # Simple reward based on distance to target
+        # dist_to_target = torch.norm(self._cubes.get_world_poses(clone=False)[0] - self._targets.get_world_poses(clone=False)[0], dim=-1)
+        # reward = -dist_to_target  # Simple reward based on distance to target
         
-        self.rew_buf[:] = reward
+        self.rew_buf[:] = self.compute_franka_reward(
+            self, self.reset_buf, self.progress_buf, self.actions, self.states, self.reward_settings, self._max_episode_length
+        )
 
+        ##### 수정 필요 #####
     def is_done(self) -> None:
         # Termination condition: reset when the cube is close to the target or max steps reached
         # get_world_poses로 정보 얻어올 수 있음.
@@ -368,51 +442,48 @@ class FrankaPickAndPlaceTask(RLTask):
         self.reset_buf = torch.where(dist_to_target < 0.05, torch.ones_like(self.reset_buf), self.reset_buf)
         self.reset_buf = torch.where(self.progress_buf >= self._max_episode_length - 1, torch.ones_like(self.reset_buf), self.reset_buf)
 
-@torch.jit.script
-def compute_franka_reward(
-    reset_buf, progress_buf, actions, states, reward_settings, max_episode_length
-):
-    
-    # Compute per-env physical parameters
-    target_height = states["target_size"] + states["cube_size"] / 2.0
-    cube_size = states["cube_size"]
-    target_size = states["target_size"]
+    # @torch.jit.script
+    def compute_franka_reward(self, reset_buf, progress_buf, actions, states, reward_settings, max_episode_length):
+        # Compute per-env physical parameters
+        target_height = states["target_size"] + states["cube_size"] / 2.0
+        cube_size = states["cube_size"]
+        target_size = states["target_size"]
 
-    # distance from hand to the cube
-    d = torch.norm(states["cube_pos_relative"], dim=-1)
-    d_lf = torch.norm(states["cube_pos"] - states["eef_lf_pos"], dim=-1)
-    d_rf = torch.norm(states["cube_pos"] - states["eef_rf_pos"], dim=-1)
-    dist_reward = 1 - torch.tanh(10.0 * (d + d_lf + d_rf) / 3)
+        # distance from hand to the cube
+        d = torch.norm(states["cube_eef_pos"], dim=-1)
+        d_lf = torch.norm(states["cube_pos"] - states["eef_lf_pos"], dim=-1)
+        d_rf = torch.norm(states["cube_pos"] - states["eef_rf_pos"], dim=-1)
+        dist_reward = 1 - torch.tanh(10.0 * (d + d_lf + d_rf) / 3)
 
-    # reward for  lifting cube
-    cube_height = states["cube_pos"][2] - reward_settings["table_height"]
-    cube_lifted = (cube_height - cube_size) > 0.04
-    lift_reward = cube_lifted
+        # reward for  lifting cube
+        cube_height = states["cube_pos"][2] - reward_settings["table_height"]
+        cube_lifted = (cube_height - cube_size) > 0.04
+        lift_reward = cube_lifted
 
-    # how closely aligned cube is to target (only provided if cube is lifted)
-    offset = torch.zeros_like(states["cube_to_target_pos"])
-    offset[2] = (cube_size + target_size) / 2
-    d_at = torch.norm(states["cube_to_target_pos"] + offset, dim=-1)
-    align_reward = (1 - torch.tanh(10.0 * d_at)) * cube_lifted
+        # how closely aligned cube is to target (only provided if cube is lifted)
+        offset = torch.zeros_like(states["cube_target_pos"])
+        offset[2] = (cube_size + target_size) / 2
+        d_at = torch.norm(states["cube_target_pos"] + offset, dim=-1)
+        align_reward = (1 - torch.tanh(10.0 * d_at)) * cube_lifted
 
-    # Dist reward is maximum of dist and align reward
-    dist_reward = torch.max(dist_reward, align_reward)
+        # Dist reward is maximum of dist and align reward
+        dist_reward = torch.max(dist_reward, align_reward)
 
-    # final reard for stacking successfully (only if cube is close to target height and corresponding location, and gripper is not grasping)
-    cube_align_target = (torch.norm(states["cube_to_target_pos"][:2], dim=-1) < 0.02)
-    cube_on_target = torch.abs(cube_height - target_height) < 0.02
-    gripper_away_from_cube = (d>0.04)
-    stack_reward = cube_align_cube & cube_on_target & gripper_away_from_cube
+        # final reard for stacking successfully (only if cube is close to target height and corresponding location, and gripper is not grasping)
+        cube_align_target = (torch.norm(states["cube_target_pos"][:2], dim=-1) < 0.02)
+        cube_on_target = torch.abs(cube_height - target_height) < 0.02
+        gripper_away_from_cube = (d>0.04)
+        stack_reward = cube_align_target & cube_on_target & gripper_away_from_cube
 
-    # We either provide the stack reward or  the align + dist reward
-    rewards = torch.where(
-        stack_reward,
-        reward_settings["r_stack_scale"] * stack_reward, 
-        reward_settings["r_dist_scale"] * dist_reward + reward_settings["r_lift_scale"] * lift_reward + 
-        reward_settings["r_align_scale"] * align_reward,
-    )
+        # We either provide the stack reward or  the align + dist reward
+        rewards = torch.where(
+            stack_reward,
+            reward_settings["r_stack_scale"] * stack_reward, 
+            reward_settings["r_dist_scale"] * dist_reward + reward_settings["r_lift_scale"] * lift_reward + 
+            reward_settings["r_align_scale"] * align_reward,
+        )
 
-    # Compute resets
-    reset_buf = torch.where((progress_buf >= max_episode_length - 1) | (stack_reward > 0), torch.ones_like(reset_buf), reset_buf)
+        # Compute resets <- 이게 필요한가?
+        # reset_buf = torch.where((progress_buf >= max_episode_length - 1) | (stack_reward > 0), torch.ones_like(reset_buf), reset_buf)
 
-    return rewards, reset_buf
+        return rewards
